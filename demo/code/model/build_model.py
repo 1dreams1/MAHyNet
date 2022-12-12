4487ec8a9dd88b43e79914a8ed6d54dbe44d5a4d
1dreams1 <81896875+1dreams1@users.noreply.github.com>
2022-12-12 14:44:38 +0800

    Update build_model.py

--- demo/code/model/build_model.py
+++ demo/code/model/build_model.py
@@ -3,16 +3,11 @@ import keras
 import keras.callbacks
 from keras.layers import Conv1D, Activation, Dropout,regularizers
 import keras.backend
+from myattention import MyAttention
+from keras.layers.core import Flatten
 
-
-def build_model(model_template, number_of_kernel, kernel_length, input_shape, local_window_size=19):
-
-    '''
-      def bias_variable(shape):
-          initial = tf.constant(0.1, shape=shape)
-          initial=tf.Variable(initial)
-          return initial
-    '''
+from ePooling import *
+def build_model_one(model_template, number_of_kernel, kernel_length, input_shape, local_window_size=19):
 
     def Relu(x):
         return keras.backend.relu(x, alpha=0.5, max_value=10)
@@ -23,27 +18,52 @@ def build_model(model_template, number_of_kernel, kernel_length, input_shape, lo
         filters=number_of_kernel,
         padding='valid',
         strides=1))
-
-
     model_template.add(Activation(Relu))
+
     model_template.add(keras.layers.pooling.MaxPooling1D(pool_length=local_window_size,
-                                                         stride=None, border_mode='valid'))
-
-    model_template.add(Dropout(0.5))
-
+                                                         stride=None, border_mode='valid')) 
+    model_template.add(Dropout(0.7))
     model_template.add(keras.layers.GRU(
         output_dim=number_of_kernel,
         return_sequences=True))
     model_template.add(Activation(Relu))
-
     model_template.add(Multi_Head_attention(out_dim=number_of_kernel))
-
     model_template.add(keras.layers.GlobalMaxPooling1D())
-    model_template.add(Dropout(0.5))
-    model_template.add(keras.layers.core.Dense(output_dim=1, name='Dense_l1'))
-    model_template.add(keras.layers.Activation("sigmoid"))
-    sgd = keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.005, amsgrad=False)
-    return model_template, sgd
+    return model_template
+
+def build_model_two(model_template, number_of_kernel, kernel_length, input_shape, local_window_size=19):
+        def Relu(x):
+            return keras.backend.relu(x, alpha=0.5, max_value=10)
+
+        model_template.add(Conv1D(
+            input_shape=input_shape,
+            kernel_size=kernel_length,
+            filters=number_of_kernel,
+            padding='valid',
+            strides=1))
+        model_template.add(Activation(Relu))
+        model_template.add(keras.layers.pooling.MaxPooling1D(pool_length=local_window_size,
+                                                             stride=None, border_mode='valid'))
+        s = 1
+        wl = (81 - kernel_length) / s + 1
+        H1 = (wl - local_window_size) / s + 1
+        wd = (4 - kernel_length) / s + 1
+        Hd = (wd - local_window_size) / s + 1
+        input = (H1, Hd)
+
+        model_template.add(Conv1D(
+            input_shape=input,
+            kernel_size=kernel_length,
+            filters=number_of_kernel,
+            padding='same',
+            strides=1))
+
+        model_template.add(Activation(Relu))
+        model_template.add(Multi_Head_attention(out_dim=number_of_kernel))
+        model_template.add(keras.layers.GlobalMaxPooling1D())
+        model_template.add(Dropout(0.7))
+        return model_template
+
 
 
 
