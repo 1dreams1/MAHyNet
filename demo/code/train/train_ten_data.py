a67a6b5071b2c5114331fc9380ce8747d496c6b9
1dreams1 <81896875+1dreams1@users.noreply.github.com>
2022-12-12 15:33:17 +0800

    Update train_ten_data.py

--- demo/code/train/train_ten_data.py
+++ demo/code/train/train_ten_data.py
@@ -4,18 +4,22 @@ import sys
 import glob
 import time
 
-def run_data(data_prefix, result_path, data_info, GPU_SET, kernel_number, local_window_size,random_seed):
+def run_data(data_prefix, data_prefix1,result_path ,data_info, data_info1, GPU_SET, kernel_number, local_window_size,random_seed):
+
 
     cmd = "python ../model/train_model_all.py"
     data_path = data_prefix +"\\" + data_info + "\\"
-    set = cmd + " " + data_path + " " +result_path + " " + data_info + " "+str(kernel_number) + " " + str(random_seed) +" " + str(local_window_size)+ " " +GPU_SET
+    data_path1 = data_prefix1 + "\\" + data_info1 + "\\"
+    set = cmd + " " + data_path + " " + data_path1 + " " + result_path + " " + data_info + " "+str(kernel_number) + " " + str(random_seed) +" " + str(local_window_size)+ " " +GPU_SET
     print(set)
     os.system(set)
+
+
 
 def get_data_info(path):
     """
     :param path:  the data path
-    :return: a list include  all chip data name (total 53 data set)
+    :return: a list include  all chip data name (total 690 data set)
     """
     #path_list = glob.glob(path + '*/')
     path_list = glob.glob(path)
@@ -28,41 +32,44 @@ def get_data_info(path):
 
 if __name__ == '__main__':
 
-    # GPU_SET: which GPU to use
-    # start : the start in this running
-    # end: the end in this running
-
-
     GPU_SET = sys.argv[1]
     start = int(sys.argv[2])
     end = int(sys.argv[3])
     '''
     GPU_SET = 0
     start = 0
-    end = 53
+    end = 2
     '''
     # the path of data
     path = r"F:\Download\lengent\venv\MAHyNet-main\demo\HDF510\*"
     data_prefix = r"F:\Download\lengent\venv\MAHyNet-main\demo\HDF510"
-    result_path = r"F:\Download\lengent\venv\MAHyNet-main\demo\result_no"
+    result_path = r"F:\Download\lengent\venv\MAHyNet-main\demo\result_pppppp"
+
+    path1 = r"F:\Download\lengent\venv\MAHyNet-main\demo\HDF5li\*"
+    data_prefix1 = r"F:\Download\lengent\venv\MAHyNet-main\demo\HDF5li"
+
     data_list = get_data_info(path)
-    print(data_list)
+    data_list1 =get_data_info(path1)
     start_time =  time.time()
-
-
-    pool = Pool(processes  = 1)
+    pool = Pool(processes  = 4)
     local_window_size_list = [19]
     random_seed_list = [1]
-    kernal_number_list = [128]
+    kernal_number_list = [256]
 
     print("start run all models")
     for kernel_number in kernal_number_list:
         for random_seed in random_seed_list:
             for local_window_size in local_window_size_list:
                 for data_info in data_list[start:end]:
-                        print(data_info, GPU_SET, local_window_size, random_seed)
-                        time.sleep(2)
-                        pool.apply_async(run_data, (data_prefix, result_path, data_info, GPU_SET, kernel_number, local_window_size, random_seed))
+                   for data_info1 in data_list1[start:end]:
+                       if  data_info==data_info1:
+
+                            print(data_info, GPU_SET, local_window_size, random_seed)
+                            print(data_info1, GPU_SET, local_window_size, random_seed)
+                            time.sleep(2)
+
+                            pool.apply_async(run_data, (data_prefix, data_prefix1,result_path ,data_info, data_info1, GPU_SET, kernel_number, local_window_size, random_seed))
     pool.close()
     pool.join()
     print("all model cost ",  time.time() - start_time)
+
