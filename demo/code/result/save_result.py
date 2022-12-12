c2aedf96e7c5edd9f08cf1b6c64c2e00faff48bc
1dreams1 <81896875+1dreams1@users.noreply.github.com>
2022-12-12 15:24:53 +0800

    Update save_result.py

--- demo/code/result/save_result.py
+++ demo/code/result/save_result.py
@@ -1,13 +1,16 @@
 import glob
+from sklearn.metrics import average_precision_score
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import h5py
-#from tqdm import tqdm
 import sklearn
 from sklearn.metrics import roc_auc_score, auc, roc_curve
 import seaborn as sns
 import pickle
+from scipy import stats
+from scipy.stats import kstest
 
 def get_label(rec):
     data = h5py.File(rec, 'r')
@@ -15,19 +18,17 @@ def get_label(rec):
     sequence_code = data['sequences']
     label = data['labs']
     return label
-
+ls = []
 def AUC(label, pred):
     roc_auc_score(label, pred)
     false_positive_rate, true_positive_rate, thresholds = roc_curve(label, pred)
-    #print(false_positive_rate,'***',true_positive_rate,'^^^^^^', thresholds,'%%%%%%%%%%')
     roc_auc = sklearn.metrics.auc(false_positive_rate, true_positive_rate)
+    print(roc_auc)
     return roc_auc
-
 
 def get_model_result(result_path, data_path):
     path = result_path
     result_list = glob.glob(path + '\\*.npy')
-
     auc = []
     random_seed = []
     kernel_length_list=[]
@@ -41,8 +42,6 @@ def get_model_result(result_path, data_path):
         label = get_label(label_path)
         label = np.array(label)
         pred = np.load(rec)
-
-
         local_window.extend([rec.split("_")[-1].split('.')[-2]])
         random_seed.extend([rec.split("_")[-7]])
         kernel_length_list.extend([rec.split("_")[-3]])
@@ -54,15 +53,15 @@ def get_model_result(result_path, data_path):
 #You need modify path
 result_path = r"F:\Download\lengent\venv\MAHyNet-main\demo\result_window\*"
 data_path = "F:\Download\lengent\\venv\MAHyNet-main\demo\HDF5\\"
-# initial the list
+
 auc = []
+ap = []
 random_seed = []
 kernel_length_list=[]
 local_window = []
 data_info_list = []
 kernel_number_list = []
 data_info_list = []
-# calculate all thing we need
 now_auc, now_random_seed, now_kernel_length_list,now_local_window,  now_data_info_list, now_kernel_number_list = get_model_result(result_path, data_path)
 
 auc.extend(now_auc)
@@ -71,7 +70,7 @@ kernel_length_list.extend( now_kernel_length_list)
 local_window.extend(now_local_window)
 data_info_list.extend(now_data_info_list)
 kernel_number_list.extend(now_kernel_number_list)
-#print(now_auc, now_random_seed, now_local_window, now_m_list, now_mode_list, now_trainable_list,  now_model_type_list, now_data_info_list, now_kernel_number_list )
+
 
 dic = {
     'data_set': data_info_list,
@@ -89,5 +88,6 @@ data_all['local_window'] = data_all['local_window'].astype(int)
 data_all['kernel_number'] = data_all['kernel_number'].astype(int)
 data_all['AUC'] = data_all['AUC'].astype(float)
 
+
 # save the result to csv file
-data_all.to_csv('result_kernel_window.csv', index=False)
+data_all.to_csv('result_2li_A2.csv', index=False)
